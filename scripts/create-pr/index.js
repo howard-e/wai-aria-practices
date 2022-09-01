d90485e299b0acbd04d18f72795d972c329cf13a
Howard Edwards <howarde.edwards@gmail.com>
2022-09-01 12:41:57 -0500

    Update workflows

--- scripts/create-pr/index.js
+++ scripts/create-pr/index.js
@@ -9,6 +9,7 @@ const jobId = process.env.JOB_ID;
 const isSuccess = process.env.OUTCOME === 'success';
 const repositoryOwner = process.env.REPO_OWNER;
 const previewLink = 'wai-aria-practices-howarde.netlify.app';
+// const previewLink = 'wai-aria-practices2.netlify.app';
 
 const updateApgPrBody = async (waiPrNumber, createPullRequestResult) => {
     // Update APG PR
@@ -55,46 +56,9 @@ const updateApgPrBody = async (waiPrNumber, createPullRequestResult) => {
     let waiPrNumber;
     let createPullRequestResult;
 
-    // const previewLink = 'wai-aria-practices2.netlify.app';
-
     if (!isSuccess) {
         try {
             await updateApgPrBody();
-
-            // Update APG PR
-            // const getApgPrResult = await octokit.rest.pulls.get({
-            //     owner: repositoryOwner,
-            //     repo: 'aria-practices',
-            //     pull_number: process.env.APG_PR_NUMBER
-            // });
-            // console.info('pull.get.success');
-            //
-            // // Needed to append error to PR body
-            // let apgPrBody = getApgPrResult.data.body || '';
-            //
-            // // Find index of existing successful or failure build links in the body
-            // let previewLinkIndex = apgPrBody.indexOf('[WAI Preview Link]');
-            // if (previewLinkIndex < 0) { // Need to account for error content
-            //     // Assumes a previous link didn't exist, find error content
-            //     previewLinkIndex = apgPrBody.indexOf('WAI Preview Link [failed to build]')
-            // }
-            //
-            // const previewLinkUrl = `https://github.com/${repositoryOwner}/wai-aria-practices/runs/${jobId}?check_suite_focus=true`;
-            // const additionalBodyContent = `WAI Preview Link [failed to build](${previewLinkUrl}) on 'Update site files' step. _(Last tried on ${new Date().toUTCString()})._`;
-            // if (previewLinkIndex < 0) { // no preview link in PR body; append
-            //     apgPrBody = `${apgPrBody}\n___\n${additionalBodyContent}`;
-            // } else { // replace existing preview link in PR body
-            //     let stringRemainder = apgPrBody.substring(previewLinkIndex);
-            //     apgPrBody = apgPrBody.replace(stringRemainder, additionalBodyContent);
-            // }
-            //
-            // await octokit.rest.pulls.update({
-            //     owner: repositoryOwner,
-            //     repo: 'aria-practices',
-            //     pull_number: process.env.APG_PR_NUMBER,
-            //     body: apgPrBody
-            // });
-            // console.info('pull.update.success.failure.block', apgPrBody);
         } catch (e) {
             console.error('octokit.call.failure.block.fail', e);
         }
@@ -134,42 +98,6 @@ const updateApgPrBody = async (waiPrNumber, createPullRequestResult) => {
         }
 
         await updateApgPrBody(waiPrNumber, createPullRequestResult);
-
-        // Update APG PR
-        // const getApgPrResult = await octokit.rest.pulls.get({
-        //     owner: repositoryOwner,
-        //     repo: 'aria-practices',
-        //     pull_number: process.env.APG_PR_NUMBER
-        // });
-        // console.info('pull.get.success');
-        //
-        // // Needed to append error to PR body
-        // let apgPrBody = getApgPrResult.data.body || '';
-        //
-        // // Find index of existing successful or failure build links in the body
-        // let previewLinkIndex = apgPrBody.indexOf('[WAI Preview Link]');
-        // if (previewLinkIndex < 0) { // Need to account for error content
-        //     // Assumes a previous link didn't exist, find error content
-        //     previewLinkIndex = apgPrBody.indexOf('WAI Preview Link [failed to build]')
-        // }
-        //
-        // let previewLinkUrl = `https://deploy-preview-${waiPrNumber || createPullRequestResult.data.number}--${previewLink}`;
-        // const additionalBodyContent = `[WAI Preview Link](${previewLinkUrl}) _(Last built on ${new Date().toUTCString()})._`;
-        // if (previewLinkIndex < 0) { // no preview link in PR body; append
-        //     apgPrBody = `${apgPrBody}\n___\n${additionalBodyContent}`;
-        // } else { // replace existing preview link in PR body
-        //     let stringRemainder = apgPrBody.substring(previewLinkIndex);
-        //     apgPrBody = apgPrBody.replace(stringRemainder, additionalBodyContent);
-        // }
-        //
-        // // creates preview link in aria-practices PR
-        // await octokit.rest.pulls.update({
-        //     owner: repositoryOwner,
-        //     repo: 'aria-practices',
-        //     pull_number: process.env.APG_PR_NUMBER,
-        //     body: apgPrBody
-        // });
-        // console.info('pull.update.success.success.block', apgPrBody);
     } catch (e) {
         console.error('octokit.call.success.block.fail', e);
     }
